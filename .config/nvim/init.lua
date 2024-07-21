383dd47b719b1ba6cdf6576c6627125be9a4bcd0
sith98 <sith9809@gmail.com>
2024-07-21 13:43:14 +0200

    2024-07-21

---
+++ .config/nvim/init.lua
@@ -0,0 +1,7 @@
+vim.opt.scrolloff = 10
+
+vim.g.mapleader = " "
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
